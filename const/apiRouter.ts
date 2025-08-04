cb8f956c6872d3b6297cd307c6d8cadb528e51ef
Quang Linh <quanglink62@gmail.com>
2025-08-04 17:41:51 +0700

    04/08/2025 lai

--- const/apiRouter.ts
+++ const/apiRouter.ts
@@ -7,7 +7,14 @@ CREATE_USERNAME:"/api/v1/users",
 EDIT_USERNAME:"/api/v1/users/{user_id}",
 /////Roles
 GET_LIST_ROLES:"/api/v1/roles/",
+CREATE_ROLES:"/api/v1/roles/",
+EDIT_ROLES:"/api/v1/roles/{user_id}",
+DELETE_ROLES: "/api/v1/roles/{user_id}",
+///system
 GET_LIST_SYSTEM:"/api/v1/system/",
+CREATE_SYSTEM:"/api/v1/system/",
+EDIT_SYSTEM:"/api/v1/system/{user_id}",
+DELETE_SYSTEM: "/api/v1/system/{user_id}",
 
 
 
